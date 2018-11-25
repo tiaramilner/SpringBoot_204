a6301e5ae7e420becc8d793c78deffdc2f904a5d
Tiara Milner <tiaramilner@MacBook-Air-5.local>
2018-11-24 22:38:25 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/SpringBoot204Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot204Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot204Application.class, args);
+    }
+}
